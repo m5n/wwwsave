94f23e119f086de4b06b797f617ac017ebc41655
m5n <m5n.github@gmail.com>
2014-11-24 18:42:05 -0800

    Refactor: use attr_reader/alias_method.

--- lib/wwwsave/logger.rb
+++ lib/wwwsave/logger.rb
@@ -1,15 +1,14 @@
 module WWWSave
   class Logger
+    attr_reader :verbose
+    alias_method :verbose?, :verbose
+
     def initialize(verbose)
       @verbose = verbose
     end
 
     def log(str)
-      puts "LOG: #{str}" if @verbose
-    end
-
-    def verbose?
-      @verbose
+      puts "LOG: #{str}" if verbose?
     end
   end
 end
